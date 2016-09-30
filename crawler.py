94404340c168d1b9dd10f9cdc74af4e4488e0235
Mason Elmore <mason.elmore@gmail.com>
2016-09-29 22:00:03 -0500

    Add stub with an idea for general program flow

---
+++ crawler.py
@@ -0,0 +1,9 @@
+def crawl():
+    # Step 0: Fetch a random wikipedia article.
+    # Step 1: Add it to a list of visited articles and print the article title.
+    # Step 2: Find the first link in the article that leads to another article.
+    # Step 3: Check if we have already visited the article.
+    #   Yes? Quit the program.
+    #   No? Go to Step 1.
+
+    pass  # Remove this.  It's only here to make the compiler happy for now.
